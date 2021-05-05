8ca9cdde73311b5ac24b7c6ced8849c14098858f
Enggar Rahayu <enggarrahayu64@gmail.com>
2021-05-05 08:29:06 +0700

    done

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBZ9JXnhiv9-7DxKvJolni2TuCeyA_r4_I",
+    authDomain: "week11-46223.firebaseapp.com",
+    databaseURL: "https://week11-46223-default-rtdb.firebaseio.com",
+    projectId: "week11-46223",
+    storageBucket: "week11-46223.appspot.com",
+    messagingSenderId: "445706952701",
+    appId: "1:445706952701:web:110c95ebee738c9eb38394",
+    measurementId: "G-12TBVTK0ML"
+  };
+
+  export default firebaseConfig;
